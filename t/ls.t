71d1ebcc8cdc44db95114f541ab7ee2053084560
Pavel Vasilyev <pvasilev@astralinux.ru>
2020-05-29 03:09:38 +0300

    RE

--- t/ls.t
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NR=0
-
-. ./testlib.sh
-
-echo 1..1
-
-t "ls / >/dev/null" "ls works"
--- t/testlib.sh
+++
@@ -1,8 +0,0 @@
-t() {
-    NR=$(expr $NR + 1)
-    if eval "$1"; then
-        echo "ok $NR - $2"
-    else
-        echo "not ok $NR - $2"
-    fi
-}
